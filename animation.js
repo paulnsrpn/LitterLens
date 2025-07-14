9aceacfef379f064652ad22f7ddca5a4f4e8ea52
paulnsrpn <serapion_pauline@plpasig.edu.ph>
2025-07-14 15:39:52 +0800

    Progress

---
+++ animation.js
@@ -0,0 +1,6 @@
+<script>
+  document.querySelector('.inner-c').addEventListener('click', function () {
+    document.getElementById('upload-sec').scrollIntoView({ behavior: 'smooth' })
+  });
+</script>
+
