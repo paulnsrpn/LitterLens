1e398527412d5d3f4a8204c0203a6adda7df7261
paulnsrpn <serapion_pauline@plpasig.edu.ph>
2025-07-18 21:23:39 +0800

    New Litterlens Folder

--- animation.js
+++ animation.js
@@ -1,6 +1,15 @@
-<script>
-  document.querySelector('.inner-c').addEventListener('click', function () {
-    document.getElementById('upload-sec').scrollIntoView({ behavior: 'smooth' })
+
+  window.addEventListener("load", () => {
+    const loader = document.getElementById("loader");
+    const mainContent = document.getElementById("main-content");
+
+    setTimeout(() => {
+      loader.classList.add("hide");
+
+      setTimeout(() => {
+        loader.style.display = "none";
+        mainContent.style.display = "block";
+      }, 1000); // matches the fade transition
+    }, 4000); // Adjust loading time (ms)
   });
-</script>
 
